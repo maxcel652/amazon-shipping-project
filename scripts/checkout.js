0626b2e83ecccb64bccf6a4e7a039798cba58593
maxcel652 <mukemmbawei@gmail.com>
2025-03-06 16:53:00 +0100

    add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,8 @@
 import { renderOrderSummary } from "./checkout/Orderummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummarry.js";
 import { loadFromStorage } from "../data/cart.js";
-import '../data/cart-class.js'
+// import '../data/cart-class.js'
+import '../data/backend-practice.js'
 
 renderOrderSummary();
 renderPaymentSummary()
