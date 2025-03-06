43c0761cbd627c0befa9def9821a2a95487a5b39
maxcel652 <mukemmbawei@gmail.com>
2025-03-06 01:59:18 +0100

    create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,8 @@
 import { renderOrderSummary } from "./checkout/Orderummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummarry.js";
+import { loadFromStorage } from "../data/cart.js";
+import '../data/cart-oop.js'
 
 renderOrderSummary();
 renderPaymentSummary()
+loadFromStorage();
