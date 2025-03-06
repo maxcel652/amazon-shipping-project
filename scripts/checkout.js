205b0abf72bcb463cb637c0f2e7a52f941917ad4
maxcel652 <mukemmbawei@gmail.com>
2025-03-06 11:04:56 +0100

    Create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderOrderSummary } from "./checkout/Orderummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummarry.js";
 import { loadFromStorage } from "../data/cart.js";
-import '../data/cart-oop.js'
+import '../data/cart-class.js'
 
 renderOrderSummary();
 renderPaymentSummary()
